4dd7dfc32a77a89435fb2fa681fdee863fa39736
Juan333-Major <juancrls.894@gmail.com>
2026-03-14 11:23:15 -0300

    codigo finalizado

--- src/index.js
+++ src/index.js
@@ -1,1 +1,37 @@
-console.log("Hello World")
+let ranking = calculo(50, 10)
+let nivel = ""
+
+if(ranking <= 10){
+    nivel = "Ferro"
+    console.log(`O Herói tem de vitórias ${ranking} e está no nível ${nivel}`)
+
+}else if(ranking <= 20){
+    nivel = "Bronze"
+    console.log(`O Herói tem de vitórias ${ranking} e está no nível ${nivel}`)
+    
+}else if(ranking <= 50){
+    nivel = "Prata"
+    console.log(`O Herói tem de vitórias ${ranking} e está no nível ${nivel}`)
+
+}else if(ranking <= 80){
+    nivel = "Ouro"
+    console.log(`O Herói tem de vitórias ${ranking} e está no nível ${nivel}`)
+
+}else if(ranking <= 90){
+    nivel = "Diamante"
+    console.log(`O Herói tem de vitórias ${ranking} e está no nível ${nivel}`)
+
+}else if(ranking <= 100){
+    nivel = "Lendário"
+    console.log(`O Herói tem de vitórias ${ranking} e está no nível ${nivel}`)
+
+}else{
+    nivel = "Imortal"
+    console.log(`O Herói tem de vitórias ${ranking} e está no nível ${nivel}`)
+}
+
+function calculo(vitoria, derrota){
+    let resultado = vitoria - derrota
+    return resultado
+
+}
